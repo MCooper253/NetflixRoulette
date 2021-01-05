ed13e9dc4d56894b5a36065779dd6f45afd33ddf
MCooper253 <morgan_cooper@epam.com>
2021-01-05 12:52:59 +0000

    adds a few more movies to the state

--- src/app/Containers/Body.js
+++ src/app/Containers/Body.js
@@ -55,6 +55,33 @@ class Body extends React.Component {
                 year: '2000',
                 pictureURL: PointBreakImg,
                 id: '05'
+            },
+            {
+                name: 'Pulp Fiction',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                genre: 'crime',
+                descriptionShort: 'Action & Adventure',
+                year: '2004',
+                pictureURL: PulpFictionImg,
+                id: '06'
+            },
+            {
+                name: 'Pulp Fiction',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                genre: 'crime',
+                descriptionShort: 'Action & Adventure',
+                year: '2004',
+                pictureURL: PulpFictionImg,
+                id: '07'
+            },
+            {
+                name: 'Pulp Fiction',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                genre: 'crime',
+                descriptionShort: 'Action & Adventure',
+                year: '2004',
+                pictureURL: PulpFictionImg,
+                id: '08'
             }]
         }
     }
