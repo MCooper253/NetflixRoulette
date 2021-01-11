9ce86ed3b7d96a2de69cc4e76a6572edd8fdc772
MCooper253 <morgan_cooper@epam.com>
2021-01-11 16:50:45 +0000

    builds out functinality for add movie genres dropdown box

--- src/app/Components/EditMovieForm.js
+++ src/app/Components/EditMovieForm.js
@@ -1,31 +1,101 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '../Components/Button.js'
 
-const EditMovieForm = (props) => {
+import ModalDropdown from '../Components/ModalDropdown.js'
 
-    return (
-        <form onSubmit={props.onSubmit}>
-            <label>MOVIE ID</label><br />
-            <p>MOVIEID#43895492305432</p><br />
-            <label>TITLE</label><br />
-            <input type='text' placeholder='Title' /><br />
-            <label>RELEASE DATE</label><br />
-            <input type='text' placeholder='Select date' /><br />
-            <label>MOVIR URL</label><br />
-            <input type='text' placeholder='Movie URL here' /><br />
-            <label>GENRE</label><br />
-            <select>
-                <option>Action</option>
-                <option>Crime</option>
-            </select><br />
-            <label>OVERVIEW</label><br />
-            <input type='text' placeholder='Overview here' /><br />
-            <label>RUNTIME</label><br />
-            <input type='text' placeholder='Runtime here' /><br />
-            <input type='submit' value='CONFIRM' /> 
-            <input type='button' value='RESET' />
-        </form>
-    )
+class EditMovieForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectOptionsShown: false,
+            selectedGenres: [],
+            actionSelected: false,
+            horrorSelected: false,
+            crimeSelected:  false,
+        }
+
+        this.toggleDropdown = this.toggleDropdown.bind(this);
+        this.toggleGenreState = this.toggleGenreState.bind(this);
+        this.updateDisplayedGenres = this.updateDisplayedGenres.bind(this);
+    }
+
+    toggleDropdown () {
+        this.setState({selectOptionsShown: !this.state.selectOptionsShown})
+        //document.getElementsByClassName('genres-select-button')[0].style.color = 'white';
+
+    }
+
+    toggleGenreState (e) {
+        const genre = e.target.title;
+        const checkboxBackground = document.querySelectorAll(`li[title="${genre}"] img`)[0]; //selects the chekbox image
+
+        //toggles the state
+        this.setState({[`${genre}Selected`]: !this.state[`${genre}Selected`]})
+
+        !this.state[`${genre}Selected`] ?
+            this.setState({selectedGenres: this.state.selectedGenres.concat(genre)}):
+            this.setState({selectedGenres: this.state.selectedGenres.filter(input => input !== genre)});
+
+        //The below statement is negated (oposed to how you think the logic should be) as it runs before the state is changed becasue .setState is async.
+        !this.state[`${genre}Selected`] ? checkboxBackground.style.backgroundColor='#F65261' : checkboxBackground.style.backgroundColor='white';
+    }
+
+    updateDisplayedGenres (genreList) {
+
+        // THIS FUNCTION TAKES THE ARRAY STORED IN STATE, FORMATS AND DISPLAYS IT IN THE APP.
+        if (genreList.length !== 0)  {
+            let arrayToDisplay = [];
+            document.getElementsByClassName('genres-select-button')[0].style.color = 'white';
+            genreList.forEach(element => {
+                let genre = element.split('');
+                genre[0] = genre[0].toUpperCase();
+                arrayToDisplay.push(genre.join(''))
+            })
+
+            document.getElementsByClassName('genres-select-button')[0].innerHTML = arrayToDisplay.join(', ');
+        } else {
+            document.getElementsByClassName('genres-select-button')[0].innerHTML = 'Select genres';
+            document.getElementsByClassName('genres-select-button')[0].style.color = 'rgba(255, 255, 255, 0.3)';
+        };
+    }
+
+    componentDidUpdate() {
+
+        this.updateDisplayedGenres(this.state.selectedGenres);
+    }
+
+    render () {
+        return (
+            <form onSubmit={this.props.onSubmit}>
+                <label>MOVIE ID</label><br />
+                <p>MOVIEID#43895492305432</p><br />
+                <label>TITLE</label><br />
+                <input type='text' placeholder='Title' /><br />
+                <label>RELEASE DATE</label><br />
+                <input type='text' placeholder='Select date' /><br />
+                <label>MOVIR URL</label><br />
+                <input type='text' placeholder='Movie URL here' /><br />
+                <label>GENRE</label><br />
+                <div className='custom-select'>
+                    <Button onClick={this.toggleDropdown} className='genres-select-button' caption='Select genres' />
+                    {this.state.selectOptionsShown && <ModalDropdown
+                        toggleGenreState={this.toggleGenreState}
+                        horrorState={this.state.horrorSelected}
+                        crimeState={this.state.crimeSelected}
+                        actionState={this.state.actionSelected}
+                    />}
+                </div>
+                <label>OVERVIEW</label><br />
+                <input type='text' placeholder='Overview here' /><br />
+                <label>RUNTIME</label><br />
+                <input type='text' placeholder='Runtime here' /><br />
+                <input type='submit' value='CONFIRM' /> 
+                <input type='button' value='RESET' />
+            </form>
+        )
+    }
 }
 
 EditMovieForm.propTypes = {
---
+++ src/app/Components/ModalDropdown.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Check from '../Images/tick.svg'
+
+const ModalDropdown = (props) => {
+
+    return (
+        <div className='modal-dropdown'>
+            <ul>
+                <li onClick={props.toggleGenreState} title='action'>
+                    <img
+                        title='action'
+                        src={Check}
+                        style={props.actionState ? {backgroundColor: 'rgb(246, 82, 97)'} : null}
+                    />
+                    Action
+                </li>
+                <li onClick={props.toggleGenreState} title='crime'>
+                    <img
+                        title='crime'
+                        src={Check}
+                        style={props.crimeState ? {backgroundColor: 'rgb(246, 82, 97)'} : null}
+                    />
+                    Crime
+                </li>
+                <li onClick={props.toggleGenreState} title='horror'>
+                    <img
+                        title='horror'
+                        src={Check}
+                        style={props.horrorState ? {backgroundColor: 'rgb(246, 82, 97)'} : null}
+                    />
+                    Horror
+                </li>
+            </ul>
+        </div>
+    )
+};
+
+export default ModalDropdown;
