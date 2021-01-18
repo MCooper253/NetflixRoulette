07d246994ef5ca49f354577ddcf278ac4952f6d9
MCooper253 <morgan_cooper@epam.com>
2021-01-18 15:23:11 +0000

    converts editMovieForm from class component to functional comp w hooks.

--- src/app/Components/NavBar.js
+++ src/app/Components/NavBar.js
@@ -1,26 +1,26 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types';
 
-const NavBar = (props) => {
+const NavBar = ({ setDisplayGenre, handleOnSelect, selectedGenre }) => {
     useEffect(()=>{
         toggleNavStyle()
     })
 
     const toggleNavStyle = () => {
         document.querySelectorAll('.navBarWrapper li').forEach(input => {input.classList.remove('active')})
-        document.querySelector(`.navBarWrapper li[name = ${props.selectedGenre}]`).classList.add('active')
+        document.querySelector(`.navBarWrapper li[name = ${selectedGenre}]`).classList.add('active')
     }
 
     return(
         <section className='navBarWrapper'>
             <ul>
-                <li key='all' name='all' className='catagory active' onClick={props.setDisplayGenre}>ALL</li>
-                <li key='documentary' name='documentary' className='catagory' onClick={props.setDisplayGenre}>DOCUMENTARY</li>
-                <li key='comedy' name='comedy' className='catagory' onClick={props.setDisplayGenre}>COMEDY</li>
-                <li key='horror' name='horror' className='catagory' onClick={props.setDisplayGenre}>HORROR</li>
-                <li key='crime' name='crime' className='catagory' onClick={props.setDisplayGenre}>CRIME</li>
+                <li key='all' name='all' className='catagory active' onClick={setDisplayGenre}>ALL</li>
+                <li key='documentary' name='documentary' className='catagory' onClick={setDisplayGenre}>DOCUMENTARY</li>
+                <li key='comedy' name='comedy' className='catagory' onClick={setDisplayGenre}>COMEDY</li>
+                <li key='horror' name='horror' className='catagory' onClick={setDisplayGenre}>HORROR</li>
+                <li key='crime' name='crime' className='catagory' onClick={setDisplayGenre}>CRIME</li>
                 <li key='select' name='select'>
-                    <select onChange={props.handleOnSelect}>
+                    <select onChange={handleOnSelect}>
                         <option>RELEASE DATE</option>
                         <option>BUDGET</option>
                         <option>RUNTIME</option>
