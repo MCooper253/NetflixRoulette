9e7b598cea0981187702b635b2ca0602aa2832aa
MCooper253 <morgan_cooper@epam.com>
2021-01-27 11:23:11 +0000

    webpack rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,7 @@ const htmlPlugin = new HtmlWebPackPlugin({
 module.exports = () => {
 
     const env = dotenv.config().parsed;
-  
-    // reduce it to a nice object, the same as before
+
     const envKeys = Object.keys(env).reduce((prev, next) => {
       prev[`process.env.${next}`] = JSON.stringify(env[next]);
       return prev;
