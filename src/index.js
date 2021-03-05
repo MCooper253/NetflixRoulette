12525b2eeb3afb97d9494689cb587e7ca7d8cb8a
MCooper253 <morgan_cooper@epam.com>
2021-03-05 10:10:03 +0000

    adds Redux Store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Providor } from "react-redux"
 import { render } from "react-dom";
 
 import App from "./app/Containers/App.js"
 import './app/Styles/style.scss'
 
-render(<App />, document.getElementById("root"));
+render(
+        <Providor store={store}>
+            <App />
+        </Providor>,
+        document.getElementById("root")
+        );
